5106c5dfba212f48185b8f5635e535a64505fdd6
github username <github email address>
2019-06-11 23:32:51 +0100

    A String with uppercased method has been printed to the console

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -1,6 +1,11 @@
 let name = "Audrey"
-name.uppercased()
-
 let uppercaseName = name.uppercased()
 print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
-// prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
+//let name = "Audrey"
+//name.uppercased()
+//
+//let uppercaseName = name.uppercased()
+//print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
+//// prints "Audrey... Audrey... AUDREY! WAKE UP!")
+
